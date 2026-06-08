7e1ce8c6f6fa254022947c60741c1fca906af146
André Pires <andre.pires@hylab.pt>
2026-06-08 17:58:47 +0100

    Added so server can access LAN connections

--- webapp/app.py
+++ webapp/app.py
@@ -517,4 +517,4 @@ def export_csv():
 # ---------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("FLASK_PORT", 5002)))
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("FLASK_PORT", 5002)))
